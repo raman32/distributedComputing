52b7826b7aa0c7159c9c95f54859cbb58b39f1fb
raman32 <rg.raman.ghimire@gmail.com>
2022-09-13 18:45:58 -0400

    Changed the random file  directory to data/

---
+++ backend/distributed-computed-api-server/generateRandomNumbers.ts
@@ -0,0 +1,48 @@
+    import fs from 'fs';
+    function generateMFileWithRandomNumbers(M: number, min: number, max: number) {
+        let currentTimeStamp = Date.now();
+        createDirSync(`data/${currentTimeStamp.toString()}`);
+        for (let index = 0; index < M; index++) {
+            generateNRandomNumbersAndSaveToDisk(min + Math.floor(Math.random() * (max - min)), `${index}.csv`, `data/${currentTimeStamp.toString()}`, () => {
+                console.log("Created Random Number File")
+            });
+        }
+        return `data/${currentTimeStamp.toString()}`;
+    }
+
+    function generateNRandomNumbersAndSaveToDisk(N: number, fileName: string, directoryName: string, onFinish: () => void) {
+        var fd = fs.openSync(`./${directoryName}/${fileName}`, 'w');
+        fs.closeSync(fs.openSync(`./${directoryName}/${fileName}`, 'w'));
+        let writeStream = fs.createWriteStream(`./${directoryName}/${fileName}`);
+        let index = 0;
+        function write() {
+        let isOk = true
+        while(index < N && isOk) {
+        if(index == N - 1) {
+            writeStream.write(Math.random().toString() + ",");
+            writeStream.end();
+        }
+        else {
+            isOk = writeStream.write(Math.random().toString() + ",");
+        }
+        index++;
+        }
+        if(index < N -1 ) {
+            writeStream.once('drain', write);
+        }
+        } 
+        writeStream.on('finish',onFinish)
+        write();    
+    }
+    function createDirSync(directoryName: string) {
+        directoryName.split('/').reduce((prev, curr) => {
+        prev += curr + '/';
+        if (!fs.existsSync(prev)) {
+            fs.mkdirSync(prev);
+        }
+        return prev;
+    },'')
+    }
+    export {
+        generateMFileWithRandomNumbers
+    }
--- backend/distributed-computed-api-server/index.ts
+++ backend/distributed-computed-api-server/index.ts
@@ -1,4 +1,5 @@
 import express, {Express, Request, Response} from 'express';
+import {generateMFileWithRandomNumbers} from './generateRandomNumbers';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -9,6 +10,55 @@ const port = process.env.PORT;
 app.get('/',(req: Request,res: Response)=> {
     res.send('Distributed Computing Api Server.');
 });
+
+app.get('/generateRandomFile',(req: Request,res: Response)=> {
+    var numberOfFiles = 10;
+    if(req.query.numberOfFiles) {
+    numberOfFiles = parseInt(req.query.numberOfFiles as string);
+    }
+    if(numberOfFiles > 50) {
+        res.send('Error too many files to handle')
+        return;
+    }
+    let fileDirectory  = generateMFileWithRandomNumbers(numberOfFiles,10,1000);
+    var filePaths = [];
+    for (let index = 0; index < numberOfFiles; index++) {
+        filePaths.push(`./${fileDirectory}/${index}.csv`);
+    }
+    res.json({
+        numberOfFiles: numberOfFiles,
+        filePaths: filePaths,
+    });
+});
+
+app.post('/getFile',(req: Request,res:Response) => {
+    res.sendFile(req.params.filePath);
+})
+
+app.get('/getAllServerStatus',(req:Request, res:Response)=>{ 
+    let numberOfServers = 10;
+    var serverStatus = []
+    for (let index = 0; index < numberOfServers; index++) {
+        serverStatus.push({
+            name: '',
+            id: '',
+            currentTaks: null,
+            isAvailable: true,
+            lastTaskRunningTime: 0,
+            lastTaskFailed: false,
+            errorCode: 0,
+        })
+    }
+    res.json({
+        numberOfServers: numberOfServers,
+        serverStatus: serverStatus
+    })
+    })
+
+app.post('/setServerStatus',(req: Request, res: Response)=> {
+    // set Server Status to available or failed
+})
+
 
 app.listen(port,() => {
     console.log(`Started server at port ${port}`);
