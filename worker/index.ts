ede4d890ce867eb59ebb6a6090ccf622c3610a0c
raman32 <rg.raman.ghimire@gmail.com>
2022-09-15 02:47:20 -0400

    added support for send and recieve task and result

--- worker/index.ts
+++ worker/index.ts
@@ -1,11 +1,12 @@
-import express, {Express, Request, Response} from 'express';
+import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 
-import {downloadFileAndSaveToTemp} from './utils';
-
+import { calculateAverage, downloadFileAndSaveToTemp } from './utils';
+import { recieveTaskFromSQS } from './recieveTaskFromSQS';
+import {sendResultToSQS} from './sendResultToSQS'
 dotenv.config();
 
-const app:Express = express();
+const app: Express = express();
 const port = process.env.PORT;
 
 const cors = require('cors');
@@ -14,16 +15,60 @@ app.use(cors({
 }));
 app.use(express.json())
 
-app.post('/downloadfile',async (req: Request,res: Response)=> {
-   await downloadFileAndSaveToTemp(req.body.url,req.body.filePath);
-   res.json({
-    sucess: true
-   })
+
+var currentFilePath = '';
+var isBusy = false;
+var lastMessageID = '';
+
+setInterval(() => recieveTaskFromSQS()
+        .then((filePath) => {
+            currentFilePath = filePath as string;
+            isBusy =true
+            return downloadFileAndSaveToTemp("http://localhost:8000", filePath as string)})
+        .then(() => calculateAverage())
+        .then(({average,currentCount}) => sendResultToSQS(JSON.stringify({average,currentCount,currentFilePath})))
+        .then((message)=>{
+            lastMessageID = message as string
+            isBusy =false;
+            currentFilePath = '';
+        })
+, 120000);
+
+app.post('/downloadfile', async (req: Request, res: Response) => {
+    await downloadFileAndSaveToTemp(req.body.url, req.body.filePath);
+    res.json({
+        sucess: true
+    })
 });
-app.get('/',(req: Request,res: Response)=> {
+
+app.get('/average', async (req: Request, res: Response) => {
+    let data = await calculateAverage();
+    res.json({
+        average: data.average,
+        count: data.currentCount
+    });
+});
+app.get('/', (req: Request, res: Response) => {
     res.send('Distributed Computing Worker Node Server. A Python process can be spawned in this node');
 });
 
-app.listen(port,() => {
+
+app.get('/status', function (req, res) {
+    res.writeHead(200, {
+        'Content-Type': 'text/event-stream',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive'
+    })
+    status(res)
+})
+
+function status(res: Response) {
+    let id = `id: ${Date.now()}`;
+    console.log("Sending server status")
+    res.write(`event:message\nid: ${id}\ndata: active \n\n`)
+    setTimeout(() => status(res), 5000)
+}
+
+app.listen(port, () => {
     console.log(`Started server at port ${port}`);
 });
